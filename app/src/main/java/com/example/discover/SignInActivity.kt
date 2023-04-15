3b0bb98a1631c54455348448424b532e5f1ff144
Felix Odhiambo <fodhiambo1@fintech-group.com>
2023-04-15 15:01:59 +0300

    Both Sign up and Sign in activities are ready

---
+++ app/src/main/java/com/example/discover/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.example.discover
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
